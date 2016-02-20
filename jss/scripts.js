8ba125ffb1f63af9c338270914cfe70178915db5
Priya Vasudevan <satipriya99@gmail.com>
2016-02-20 10:48:37 -0800

    have a problem of results not being displayed.

--- jss/scripts.js
+++ jss/scripts.js
@@ -2,54 +2,44 @@ $(document).ready(function() {
 
   $("#vacationForm").submit(function(event) {
     var gender = $("select#gender").val();
-    var activity = $("select#activity").val();
     var attitude = $("select#attitude").val();
     var budget = $("select#budget").val();
-    var altitude = $("select#altitude").val();
-    var total = 0;
+    var activities = $("select#activities").val();
 
-    var activityArray = ["London, Paris, Bangkok", "Thailand, Puerto Vallarta, Aruba", "Cambodia, Egypt, Jordan", "New Delhi, South Africa, Mexico", "Tibet, Kenya, Peru"];
-    var arrayFields = [activity, adventure, relax, budget, altitude];
-    for (i = 1; i < arrayFields.length; i++) {
-      if (arrayFields[i] == "true") {
-         total = total + 1;
-      }
-    }
 
-      $("#vacationName").empty().append(activityArray[total]);
-      $("#result").show();
-
-    if (gender === 'male' && age === 'youth' && attitude === 'relax' && budget === 'above-$5,000' && activities === 'outdoor') {
+    if (gender === 'male' && attitude === 'relax' && budget === 'above-$5,000' && activities === 'outdoor') {
       $("#destination2").show()
-    } else {
-        $("#destination2").hide();
+    } //else {
+        //$("#destination2").hide();
     }
     //
     //
-    if (gender === 'male' && age === 'adult' && attitude === 'party' && budget === 'above-$5,000' && activities === 'outdoor') {
+    if (gender === 'male' && attitude === 'party' && budget === 'above-$5,000' && activities === 'outdoor') {
       $("#destination3").show()
-    } else {
-        $("#destination3").hide();
+    } //else {
+        //$("#destination3").hide();
     }
     //
-    if (gender === 'female' && age === 'adult' && attitude === 'both' && budget === 'above-$5,000' && activities === 'shopping') {
+    if (gender === 'female' && attitude === 'none' && budget === 'above-$5,000' && activities === 'shopping') {
       $("#destination3").show()
-    } else {
-        $("#destination3").hide();
+    //} else {
+        //$("#destination3").hide();
     }
     //
-    if (gender === 'male' && age === 'adult' && attitude === 'both' && budget === 'under-$5,000' && activities === 'shopping') {
+    if (gender === 'male' && attitude === 'none' && budget === 'under-$5,000' && activities === 'shopping') {
       $("#destination1").show()
-    } else {
-        $("#destination1").hide();
+    } //else {
+      //  $("#destination1").hide();
     }
     //
-    if (gender === 'female' && age === 'adult' && attitude === 'relax' && budget === 'under-$5,000' && activities === 'outdoor') {
+    if (gender === 'female' && attitude === 'relax' && budget === 'under-$5,000' && activities === 'outdoor') {
       $("#destination3").show()
-    } else {
-        $("#destination3").hide();
+    } //else {
+        //$("#destination3").hide();
     }
     //
+
+
     event.preventDefault();
 
  });
