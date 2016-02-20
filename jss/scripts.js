5187dd038bdc623616a274ddb7364235f93dfa4d
Priya Vasudevan <satipriya99@gmail.com>
2016-02-19 21:28:13 -0800

    set variables for gender activity attitude budget and altitude.

--- jss/scripts.js
+++ jss/scripts.js
@@ -2,3 +2,7 @@ $(document).ready(function() {
 
   $("#vacationForm").submit(function(event) {
     var gender = $("select#gender").val();
+    var activity = $("select#activity").val();
+    var attitude = $("select#attitude").val();
+    var budget = $("select#budget").val();
+    var altitude = $("select#altitude").val();
